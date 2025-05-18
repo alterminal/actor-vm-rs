8d410f8c5f3e72173724fdd5895d2d18a48537a5
ChenYunDa <chenyunda218@gmail.com>
2025-05-18 19:10:03 +0800

    fiuxp

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
 use std::ptr;
 
 enum Inst {
-    MOV,
-    ADD,
-    FADD,
-    SUB,
-    FSUB,
-    MUL,
+    Mov,
+    Add,
+    FAdd,
+    Sub,
+    FSub,
+    Mul,
     Div,
     And,
     Reg64,
-    LDR,
-    STR,
+    Ldr,
+    Str,
 }
 
 impl Inst {
     fn from_u8(value: u8) -> Self {
         match value {
-            0 => Inst::MOV,
-            1 => Inst::ADD,
-            2 => Inst::FADD,
-            3 => Inst::SUB,
-            4 => Inst::FSUB,
-            5 => Inst::MUL,
+            0 => Inst::Mov,
+            1 => Inst::Add,
+            2 => Inst::FAdd,
+            3 => Inst::Sub,
+            4 => Inst::FSub,
+            5 => Inst::Mul,
             6 => Inst::Div,
             7 => Inst::And,
             8 => Inst::Reg64,
-            9 => Inst::LDR,
-            10 => Inst::STR,
+            9 => Inst::Ldr,
+            10 => Inst::Str,
             _ => panic!("Unknown instruction"),
         }
     }
@@ -64,20 +64,117 @@ impl Reg {
     }
 }
 
-struct Vm {
-    m: Vec<u8>,
-    regs: [usize; 9],
+enum RV32I {
+    Ului = 55,
+    Uauipc = 23,
+    Jjal = 111,
+    Ijalr = 103,
 }
 
-impl Vm {
+enum RV64I {
+    ADD,
+    ADDI,
+    AND,
+    ANDI,
+    OR,
+    ORI,
+    XOR,
+    XORI,
+    SLL,
+    SLLI,
+    SRL,
+    SRLI,
+    SRA,
+    SRAI,
+    SUB,
+    SUBI,
+}
+
+// RISC-V general purpose registers
+enum RVG {
+    ZERO,
+    RA,
+    SP,
+    GP,
+    TP,
+    T0,
+    T1,
+    T2,
+    FP,
+    S1,
+    A0,
+    A1,
+    A2,
+    A3,
+    A4,
+    A5,
+    A6,
+    A7,
+    S2,
+    S3,
+    S4,
+    S5,
+    S6,
+    S7,
+    S8,
+    S9,
+    S10,
+    S11,
+    T3,
+    T4,
+    T5,
+    T6,
+}
+
+// RISC-V floating point registers
+enum RGF {
+    FT0,
+    FT1,
+    FT2,
+    FT3,
+    FT4,
+    FT5,
+    FT6,
+    FT7,
+    FS0,
+    FS1,
+    FA0,
+    FA1,
+    FA2,
+    FA3,
+    FA4,
+    FA5,
+    FA6,
+    FA7,
+    FS2,
+    FS3,
+    FS4,
+    FS5,
+    FS6,
+    FS7,
+    FS8,
+    FS9,
+    FS10,
+    FS11,
+    FT8,
+    FT9,
+    FT10,
+    FT11,
+}
+
+struct ActorVm {
+    m: Vec<u8>,
+    regs: [usize; 9],
+    rvi: [usize; 32],
+    rvf: [f64; 32],
+}
+
+impl ActorVm {
     fn fetch_instr(&self) -> Inst {
         Inst::from_u8(self.m[self.pc()])
     }
     fn fetch_reg(&self, offset: usize) -> Reg {
         Reg::from_u8(self.m[self.pc() + offset])
-    }
-    fn fetch(&mut self) -> u8 {
-        self.m[self.pc()]
     }
     fn fetch_8(&self, addr: usize) -> u8 {
         let ptr = &self.m[addr] as *const u8;
@@ -117,17 +214,20 @@ impl Vm {
     fn tick(&mut self) {
         let inst = self.fetch_instr();
         match inst {
-            Inst::ADD => {
+            Inst::Add => {
                 self.add();
             }
             Inst::Reg64 => {
                 self.reg_64();
             }
-            Inst::MOV => {
+            Inst::Mov => {
                 self.mov();
             }
-            Inst::LDR => {
+            Inst::Ldr => {
                 self.ldr();
+            }
+            Inst::Str => {
+                self.str();
             }
             _ => {
                 println!("hello:{:?}", inst as u8);
@@ -139,7 +239,16 @@ impl Vm {
         let addr = self.fetch_addr(2);
         let value = self.fetch_64(addr);
         self.set_reg(reg, value as usize);
-        self.inc_ps(9);
+        self.inc_ps(10);
+    }
+    fn str(&mut self) {
+        let reg = self.fetch_reg(1);
+        let value = self.get_reg(reg);
+        let addr = self.fetch_addr(2);
+        let ptr = &mut self.m[addr] as *mut u8;
+        let ptr = ptr as *mut usize;
+        unsafe { ptr::write(ptr, value) }
+        self.inc_ps(10);
     }
     fn add(&mut self) {
         let to = self.fetch_reg(1);
@@ -147,7 +256,7 @@ impl Vm {
         let right = self.fetch_reg(3);
         let value = self.get_reg(left) + self.get_reg(right);
         self.set_reg(to, value);
-        self.inc_ps(3);
+        self.inc_ps(4);
     }
     fn reg_64(&mut self) {
         let reg = self.fetch_reg(1);
@@ -166,50 +275,25 @@ impl Vm {
         println!("PC: {}", self.pc());
         println!("Registers: {:?}", self.regs);
     }
+    fn print_mem(&self, addr: usize, offset: usize) {
+        for i in 0..offset {
+            println!("{}: {}", addr + i, self.m[addr + i]);
+        }
+    }
     fn pc(&self) -> usize {
         self.regs[Reg::PC as usize]
     }
     fn inc_ps(&mut self, inc: usize) {
         self.regs[Reg::PC as usize] += inc;
     }
-
     fn new() -> Self {
-        Vm {
+        ActorVm {
             m: vec![0; 1024],
+            rvi: [0; 32],
+            rvf: [0.0; 32],
             regs: [0; 9],
         }
     }
 }
 
-fn main() {
-    let mut vm = Vm::new();
-    vm.m[0] = Inst::Reg64 as u8;
-    vm.m[1] = Reg::R2 as u8;
-    vm.m[2] = 255;
-    vm.m[3] = 255;
-    vm.m[4] = 255;
-    vm.m[5] = 255;
-    vm.m[6] = 0;
-    vm.m[7] = 0;
-    vm.m[8] = 0;
-    vm.m[9] = 0;
-    vm.m[10] = Inst::MOV as u8;
-    vm.m[11] = Reg::R0 as u8;
-    vm.m[12] = Reg::R2 as u8;
-    vm.m[13] = Inst::ADD as u8;
-    vm.m[14] = Reg::R1 as u8;
-    vm.m[15] = Reg::R0 as u8;
-    vm.m[16] = Reg::R2 as u8;
-    vm.m[17] = Inst::LDR as u8;
-    vm.m[18] = Reg::R6 as u8;
-    vm.m[19] = 0xFF;
-    vm.m[255] = 0xFF;
-    vm.m[256] = 0xFF;
-    vm.m[257] = 0xFF;
-    vm.m[258] = 0xFF;
-    vm.tick();
-    vm.tick();
-    vm.tick();
-    vm.tick();
-    println!("addr: {:?}", vm.m[255]);
-}
+fn main() {}
